685dd1475f5c1f9ff863b8356256b43f87fbdadc
Jason Harrington <jason.harrington@morningstar.com>
2017-06-02 13:43:55 -0500

    Add max iteration logic to sqrt, fix by gofmt

--- sqrt.go
+++ sqrt.go
@@ -8,24 +8,34 @@ import (
 )
 
 const epsilon = 1e-10
-const max_iterations = 1000
+const max_iterations = 1000 
 
 func Sqrt(x float64) float64 {
 	starting_value := float64(math.Abs(rand.NormFloat64()))
+
 	var previous_value float64
 	var current_value float64
+
 	var iteration int64
+
 	fmt.Printf("Initial value %v.\n", starting_value)
 	fmt.Printf("%10v%25v\n", "Iteration", "Current Estimate")
 	fmt.Printf("%10v%25v\n", "=========", "==================")
-	for current_value = starting_value; math.Abs(current_value-previous_value) > epsilon && iteration <= max_iterations; {
+
+	for current_value = starting_value; math.Abs(current_value-previous_value) > epsilon &&
+		iteration <= max_iterations; {
 		previous_value = current_value
-		current_value = previous_value - (previous_value*previous_value-x)/(2*previous_value)
+		current_value = previous_value -
+			(previous_value*previous_value-x)/(2*previous_value)
 		iteration++
 		fmt.Printf("%10v%25v\n", iteration, current_value)
 	}
 
-	fmt.Printf("Convergence after %v iterations.\n", iteration)
+	if iteration >= max_iterations {
+		fmt.Printf("Bailing out after %v iterations.\n", iteration)
+	} else {
+		fmt.Printf("Convergence after %v iterations.\n", iteration)
+	}
 	return current_value
 }
 
@@ -33,4 +43,3 @@ func main() {
 	rand.Seed(time.Now().Unix())
 	fmt.Println(Sqrt(4))
 }
-
