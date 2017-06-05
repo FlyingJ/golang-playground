4e0e83b4cda57f6f11318ed4cdb7e684677fb870
J Harrington <jason.m.harrington@gmail.com>
2017-06-04 19:46:56 -0500

    Commit working version of first version of rot13Reader

--- rot13Reader1.go
+++ rot13Reader1.go
@@ -6,70 +6,82 @@ import (
 	"strings"
 )
 
-var rot13 = map[string]string{
-	"a": "m",
-	"b": "n",
-	"c": "o",
-	"d": "p",
-	"e": "q",
-	"f": "r",
-	"g": "s",
-	"h": "t",
-	"i": "u",
-	"j": "v",
-	"k": "w",
-	"l": "x",
-	"m": "y",
-	"n": "z",
-	"o": "a",
-	"p": "b",
-	"q": "c",
-	"r": "d",
-	"s": "e",
-	"t": "f",
-	"u": "g",
-	"v": "h",
-	"w": "i",
-	"x": "j",
-	"y": "k",
-	"z": "l",
-	"A": "M",
-	"B": "N",
-	"C": "O",
-	"D": "P",
-	"E": "Q",
-	"F": "R",
-	"G": "S",
-	"H": "T",
-	"I": "U",
-	"J": "V",
-	"K": "W",
-	"L": "X",
-	"M": "Y",
-	"N": "Z",
-	"O": "A",
-	"P": "B",
-	"Q": "C",
-	"R": "D",
-	"S": "E",
-	"T": "F",
-	"U": "G",
-	"V": "H",
-	"W": "I",
-	"X": "J",
-	"Y": "K",
-	"Z": "L",
+var rot13map = map[byte]byte{
+	'a': 'n',
+	'b': 'o',
+	'c': 'p',
+	'd': 'q',
+	'e': 'r',
+	'f': 's',
+	'g': 't',
+	'h': 'u',
+	'i': 'v',
+	'j': 'w',
+	'k': 'x',
+	'l': 'y',
+	'm': 'a',
+	'n': 'a',
+	'o': 'b',
+	'p': 'c',
+	'q': 'd',
+	'r': 'e',
+	's': 'f',
+	't': 'g',
+	'u': 'h',
+	'v': 'i',
+	'w': 'j',
+	'x': 'k',
+	'y': 'l',
+	'z': 'm',
+	'A': 'N',
+	'B': 'O',
+	'C': 'P',
+	'D': 'Q',
+	'E': 'R',
+	'F': 'S',
+	'G': 'T',
+	'H': 'U',
+	'I': 'V',
+	'J': 'W',
+	'K': 'X',
+	'L': 'Y',
+	'M': 'Z',
+	'N': 'A',
+	'O': 'B',
+	'P': 'C',
+	'Q': 'D',
+	'R': 'E',
+	'S': 'F',
+	'T': 'G',
+	'U': 'H',
+	'V': 'I',
+	'W': 'J',
+	'X': 'K',
+	'Y': 'L',
+	'Z': 'M',
+}
+
+func rot13(b byte) byte {
+	switch {
+	case ('a' <= b && b <= 'z') || ('A' <= b && b <= 'Z'):
+		return rot13map[b]
+	default:
+		return b
+	}
 }
 
 type rot13Reader struct {
 	r io.Reader
 }
 
-func (r rot13Reader) Read(b []byte) (n int, err error) {
-	for i := range b {
-		b[i] = rot13[b[i]]
+func (r *rot13Reader) Read(b []byte) (n int, err error) {
+	// read from the reader contained in the underlying reader
+	n, err = r.r.Read(b)
+
+	for i := 0; i < n; i++ {
+		b[i] = rot13(b[i])
 	}
-	return len(b), nil
+	return
 }
 
 func main() {
